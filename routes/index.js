cbdb42573b9e22b75d768d71f1ddbfe884ef614a
Ryan John Vickramasinghe <rvickram@uwo.ca>
2021-01-16 16:15:18 -0500

    Implemented routes skeleton.

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+// controllers
+import users from '../controllers/user.js';
+// middlewares
+import { encode } from '../middlewares/jwt.js';
+
+const router = express.Router();
+
+router
+    .post('/login/:userId', encode, (req, res, next) => { });
+
+export default router;
