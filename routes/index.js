cf3897c6825e92926c90f509b6d57b0f948a67fc
Ryan Vickramasinghe <rvickram@uwo.ca>
2021-01-27 16:12:54 -0500

    Implemented the login route.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ import { encode } from '../middlewares/jwt.js';
 const router = express.Router();
 
 router
-    .post('/login/:userId', encode, (req, res, next) => { });
+    .post('/login/:userId', encode, (req, res, next) => {
+        return res
+            .status(200)
+            .json({
+                success: true,
+                authorization: req.authToken,
+            });
+     });
 
 export default router;
