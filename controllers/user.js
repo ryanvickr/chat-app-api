baa1eaf334dff1ebcb53da6177a1a1e0972151ae
Ryan Vickramasinghe <rvickram@uwo.ca>
2021-01-27 13:05:20 -0500

    Added route for creating new users.

--- controllers/user.js
+++ controllers/user.js
@@ -6,6 +6,29 @@ import UserModel, { USER_TYPES } from '../models/User.js';
 export default {
     onGetAllUsers: async (req, res) => { },
     onGetUserById: async (req, res) => { },
-    onCreateUser: async (req, res) => { },
+
+    // this function will validate and create a new user given creds
+    onCreateUser: async (req, res) => {
+        try {
+            // first make sure all fields are valid.
+            const validation = makeValidation(types => ({
+              payload: req.body,
+              checks: {
+                firstName: { type: types.string },
+                lastName: { type: types.string },
+                type: { type: types.enum, options: { enum: USER_TYPES } },
+              }
+            }));
+            if (!validation.success) return res.status(400).json(validation);
+            
+            // now actually add the user to the database
+            const { firstName, lastName, type } = req.body;
+            const user = await UserModel.createUser(firstName, lastName, type);
+            return res.status(200).json({ success: true, user });
+          } catch (error) {
+            return res.status(500).json({ success: false, error: error })
+          }
+    },
+
     onDeleteUserById: async (req, res) => { },
 }
